436c79c564b62a6ef0d7feec73b1eff5a09159f7
Luca Scalzotto <lucascalzotto@gmail.com>
2014-10-29 02:02:45 +0100

    Changed batch file

--- framework/Run Lambda Wars.bat
+++ framework/Run Lambda Wars.bat
@@ -1,12 +1,13 @@
 @echo off
-:Build
-@echo Building Lambda Wars
-cabal install
 :Choice
 choice /c QRP /n /m "[Q]uit, [R]ebuild, or [P]lay?"
 if errorlevel 3 goto Play
 if errorlevel 2 goto Build
 if errorlevel 1 goto End
+:Build
+@echo Building Lambda Wars
+cabal install
+goto Choice
 :Play
 @echo Starting Lambda Wars
 start /d "dist\build\lambda-wars" lambda-wars.exe
