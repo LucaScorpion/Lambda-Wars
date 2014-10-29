2f6b880b7a85ae631faf84ef28c8b3a0cfefc876
Luca Scalzotto <lucascalzotto@gmail.com>
2014-10-29 01:41:05 +0100

    Code cleanups in view

--- framework/src/View.hs
+++ framework/src/View.hs
@@ -14,18 +14,17 @@ import Model
 draw :: Float -> Float -> World -> Picture
 draw horizontalResolution verticalResolution (World{..})
     = pictures [
-     pictures (map drawStars stars),
-     translate (-fst cameraPos) (-snd cameraPos) 
-     (pictures  $ drawPlayer player : (map drawBullets bullets))
-     ]
+      pictures (map drawStars stars),
+      translate (-fst cameraPos) (-snd cameraPos) 
+      (pictures  $ drawPlayer player : (map drawBullets bullets))
+      ]
       where
       drawPlayer (Ship{..}) = translate
-	                                 (fst sPos)
-				                     (snd sPos)
-                                     (rotate ((-radToDeg sRot)-90) sSprite)
+	                          (fst sPos)
+                              (snd sPos)
+                              (rotate ((-radToDeg sRot) - 90) sSprite)
       drawStars ((x,y),z) = translate
-                         (worldWidth * x - 0.03 *(fst cameraPos * z))
-                         (worldHeight * y - 0.03 * (snd cameraPos * z))
-                         (color white $ circleSolid ((z + 0.3) * 1.9))
-						 
+                            (worldWidth * x - 10 * (fst cameraPos * z))
+                            (worldHeight * y - 10 * (snd cameraPos * z))
+                            (color white $ circleSolid ((z + 0.3) * 1.9))
       drawBullets (Bullet{..}) = translate (fst bPos) (snd bPos) (color red $ circleSolid 4)
